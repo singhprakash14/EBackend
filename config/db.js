d19b0dfbd56d5dd278274853c4813c7ce7b8ff0c
Home <prakashsingh20497@gmail.com>
2024-09-27 00:01:04 +0530

    added

--- config/db.js
+++ config/db.js
@@ -2,9 +2,14 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MongoDB_url);
+    await mongoose.connect(process.env.MongoDB_url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000,
+    });
+    console.log("MongoDB connected successfully");
   } catch (error) {
-    console.log(error);
+    console.log("Error connecting to MongoDB:", error);
   }
 }
 
